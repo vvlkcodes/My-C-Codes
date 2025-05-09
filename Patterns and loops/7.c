3a3532673c1d2fc72de03f6481c1ec48ab906f2c
V.Venkata Lalith Kartheek <vuppulurilalith@gmail.com>
2025-05-09 12:59:40 +0530

    Create 7.c

---
+++ Patterns and loops/7.c
@@ -0,0 +1,20 @@
+//to print triangle of alphabets.type 1.
+#include <stdio.h>
+
+int main()
+{
+    int n, m;
+    printf("enter the number of rows");
+    scanf("%d", &n);
+    // printf("enter the number of columns");
+    // scanf("%d", &m);
+    for(int i =1; i<=n;i++){//outer loop gives no. of rows .
+        int a = 65;
+        for(int j=1;j<=i;j++){//inner loop gives no. of columns.
+            printf("%c",a);
+            a++;
+        }printf("\n");
+    }
+
+    return 0;
+}
