3e794a1e86d92bf473a8f3e5ae182fa3ed7c279c
V.Venkata Lalith Kartheek <vuppulurilalith@gmail.com>
2025-05-09 12:58:19 +0530

    Create 6.c

---
+++ Patterns and loops/6.c
@@ -0,0 +1,20 @@
+//to print square or rectangle of alphabets.
+#include <stdio.h>
+
+int main()
+{
+    int n, m;
+    printf("enter the number of rows");
+    scanf("%d", &n);
+    printf("enter the number of columns");
+    scanf("%d", &m);
+    for(int i =1; i<=n;i++){//outer loop gives no. of rows .
+        int a = 65;
+        for(int j=1;j<=m;j++){//inner loop gives no. of columns.
+            printf("%c",a);
+            a++;
+        }printf("\n");
+    }
+
+    return 0;
+}
