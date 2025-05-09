14ab0b6df36b798259ec8f23f91805691b10ce5c
V.Venkata Lalith Kartheek <vuppulurilalith@gmail.com>
2025-05-09 13:24:41 +0530

    Create basic syntax of the functions.c

---
+++ Functions and pointers/basic syntax of the functions.c
@@ -0,0 +1,18 @@
+//basic syntax of functions.
+
+void functionName({
+    function statements;
+    return;
+}
+int main(){
+    functionName();//calling a function.
+}
+////////////////////////////////
+int functionName(int a, int b){//arguements or parameters
+    statements;
+    return;
+}
+int main(){
+    functionName(a, b);//calling the function.
+    return 0;
+}
