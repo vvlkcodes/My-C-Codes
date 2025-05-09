c5e861fef48084659c0714f271a0b1881f6acda3
V.Venkata Lalith Kartheek <vuppulurilalith@gmail.com>
2025-05-09 12:52:59 +0530

    Create printing alphabet and their ASCII value.c

---
+++ Basics of C/printing alphabet and their ASCII value.c
@@ -0,0 +1,10 @@
+//printing alphabet and their ASCII value.
+#include <stdio.h>
+int main(){
+    char ch;//enter an alphabet here.
+    printf("enter an alphabet: ");
+    scanf("%c", &ch);
+    printf("%d", ch);
+    
+    return 0;
+}
