0ad55a6808f0479234023394cccf2fe72ae3d508
V.Venkata Lalith Kartheek <vuppulurilalith@gmail.com>
2025-05-09 13:21:22 +0530

    Create Sample.c

---
+++ Functions and pointers/Sample.c
@@ -0,0 +1,20 @@
+//sample code for functions.
+
+#include <stdio.h>
+
+void greet(){
+    printf("HELLO WORLD\n");
+    printf("HOW ARE YOU?\n");
+    printf("THIS IS CHITTI\n");
+    return;
+}
+
+int main()
+{
+    greet();
+    greet();
+    greet();
+   
+
+    return 0;
+}
